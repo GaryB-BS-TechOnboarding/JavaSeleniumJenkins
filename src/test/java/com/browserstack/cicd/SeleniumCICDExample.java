1d55bb3344b1761c947bcb1251ca67fe6b29e86c
Gary Behan <garybehan@Garys-MacBook-Pro.local>
2021-12-31 14:27:33 +0000

    Small changes to code

--- src/test/java/com/browserstack/cicd/SeleniumCICDExample.java
+++ src/test/java/com/browserstack/cicd/SeleniumCICDExample.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.remote.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 
 public class SeleniumCICDExample {
     public static void main(String[] args) throws Exception {
@@ -15,6 +16,9 @@ public class SeleniumCICDExample {
         String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
         String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
 
+        HashMap<String, Boolean> networkLogsOptions = new HashMap<>();
+        networkLogsOptions.put("captureContent", true);
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("os", "Windows");
         capabilities.setCapability("os_version", "10");
@@ -24,6 +28,17 @@ public class SeleniumCICDExample {
         capabilities.setCapability("build", buildName); // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
         capabilities.setCapability("browserstack.local", browserstackLocal);
         capabilities.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
+        capabilities.setCapability("browserstack.networkLogs", true);
+        capabilities.setCapability("browserstack.networkLogsOptions", networkLogsOptions);
+        capabilities.setCapability("browserstack.geoLocation", "DE");
+        capabilities.setCapability("browserstack.maskCommands", "setValues, getValues, setCookies, getCookies");
+        capabilities.setCapability("browserstack.networkProfile", "edge-lossy");
+        capabilities.setCapability("browserstack.customNetwork", "1000,1000,100,1");
+
+
         driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
+        driver.get("https://www.browserstack.com");
+
+        System.out.println("TEST HAS REACHED THE END");
     }
 }
